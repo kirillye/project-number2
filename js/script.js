da0612da2c8cf73c7c7419b32abec53a7020833e
Kirill <kirill.stalker.2011@mail.ru>
2021-03-02 08:41:09 +0300

    Clider

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+   $('.slider__inner').slick({
+      speed: 1200,
+      prevArrow: '<button type="button" class="slick-prev"><img src="icons/chevron-left-solid.svg"> </button>',
+      nextArrow: '<button type="button" class="slick-next"><img src="icons/chevron-right-solid.svg"> </button>'
+  });
+});
